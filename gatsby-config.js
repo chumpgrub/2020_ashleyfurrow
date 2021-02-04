64ce052aa4cad9558793e99e00eb14d53bc19420
Mark Furrow <mark@orbitmedia.com>
2021-02-03 22:39:14 -0800

    package.json file returned to repo, favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,7 +104,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/af-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
